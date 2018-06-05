dffcf702a8a8abf7c487b9dc5e5d724a26d90b29
RRodrigoRamos <systemas.rodrigo@gmail.com>
2018-06-05 08:54:08 -0300

    ListaConvênios

--- database/seeds/ConvenioTableSeeder.php
+++ database/seeds/ConvenioTableSeeder.php
@@ -11,29 +11,82 @@ class ConvenioTableSeeder extends Seeder
      */
     public function run()
     {
-       Convenio::create([
-            'nome_convenio' => 'SulAmérica',
-            'tipo_plano' => 'Gold',
-            'n_registro' => '00212.123-AP',
-            'validade' => '2019-05-07',
-        ]);
-       Convenio::create([
-            'nome_convenio' => 'SulAmérica',
-            'tipo_plano' => 'Especial II',
-            'n_registro' => '01231234567890121',
-            'validade' => '2019-05-07',
-        ]);
-       Convenio::create([
-            'nome_convenio' => 'Amil',
-            'tipo_plano' => 'Classico',
-            'n_registro' => '63765500000000000',
-            'validade' => '2025-05-07',
-        ]);
-       Convenio::create([
-            'nome_convenio' => 'Caixa Saúde',
-            'tipo_plano' => 'Gold',
-            'n_registro' => '0018201.122-AP',
-            'validade' => '2019-05-07',
-        ]);
+Convenio::create(['nome_convenio' =>'Abapai']);
+Convenio::create(['nome_convenio' =>'Amapa Vidas']);
+Convenio::create(['nome_convenio' =>'Amei']);
+Convenio::create(['nome_convenio' =>'Amil *']);
+Convenio::create(['nome_convenio' =>'Amprev *']);
+Convenio::create(['nome_convenio' =>' Asmob']);
+Convenio::create(['nome_convenio' =>' Aspeb *']);
+Convenio::create(['nome_convenio' =>' Assefaz *']);
+Convenio::create(['nome_convenio' =>' APIWA-TA *']);
+Convenio::create(['nome_convenio' =>'Associação dos policiais Federais *']);
+Convenio::create(['nome_convenio' =>'Associação dos doadores de sangue']);
+Convenio::create(['nome_convenio' =>'Basa-Casf *']);
+Convenio::create(['nome_convenio' =>'Bradesco *']);
+Convenio::create(['nome_convenio' =>'Brasil vida']);
+Convenio::create(['nome_convenio' =>'Caixa saúde *']);
+Convenio::create(['nome_convenio' =>'Capesaude *']);
+Convenio::create(['nome_convenio' =>'Casembrapa *']);
+Convenio::create(['nome_convenio' =>'Cassi *']);
+Convenio::create(['nome_convenio' =>'Central medica *']);
+Convenio::create(['nome_convenio' =>'Clinica popular do trabalhador *']);
+Convenio::create(['nome_convenio' =>'Conab']);
+Convenio::create(['nome_convenio' =>'Condominio do msc *']);
+Convenio::create(['nome_convenio' =>'Consumabem']);
+Convenio::create(['nome_convenio' =>'Postal Saúde(correios) *']);
+Convenio::create(['nome_convenio' =>'Dimet']);
+Convenio::create(['nome_convenio' =>'E-vida(eletronorte) *']);
+Convenio::create(['nome_convenio' =>'Engeclin']);
+Convenio::create(['nome_convenio' =>'Engmed']);
+Convenio::create(['nome_convenio' =>'EPI medicina do trabalho *']);
+Convenio::create(['nome_convenio' =>'Ev cartaz *']);
+Convenio::create(['nome_convenio' =>'Fenix']);
+Convenio::create(['nome_convenio' =>'Funeraria centerpax *']);
+Convenio::create(['nome_convenio' =>'Funeraria santa rita *']);
+Convenio::create(['nome_convenio' =>'Geap *']);
+Convenio::create(['nome_convenio' =>'Globo saude']);
+Convenio::create(['nome_convenio' =>'Goldem med']);
+Convenio::create(['nome_convenio' =>'Grupo fortaleza *']);
+Convenio::create(['nome_convenio' =>'Hemocard *']);
+Convenio::create(['nome_convenio' =>'Ijoma *']);
+Convenio::create(['nome_convenio' =>'Mais saúde *']);
+Convenio::create(['nome_convenio' =>'Med saude']);
+Convenio::create(['nome_convenio' =>'Medicare']);
+Convenio::create(['nome_convenio' =>'Mega card']);
+Convenio::create(['nome_convenio' =>'New forte *']);
+Convenio::create(['nome_convenio' =>'Nipomed']);
+Convenio::create(['nome_convenio' =>'Nutriama-domestilar']);
+Convenio::create(['nome_convenio' =>'Parking shopping *']);
+Convenio::create(['nome_convenio' =>'Paulo Balbino *']);
+Convenio::create(['nome_convenio' =>'Plan assiste *']);
+Convenio::create(['nome_convenio' =>'Plasf Funerária *']);
+Convenio::create(['nome_convenio' =>'PM/BM (militares) *']);
+Convenio::create(['nome_convenio' =>'Proativa *']);
+Convenio::create(['nome_convenio' =>'Qualimax']);
+Convenio::create(['nome_convenio' =>'Rede vida']);
+Convenio::create(['nome_convenio' =>'Renascer amavida *']);
+Convenio::create(['nome_convenio' =>'Sesc alimentos *']);
+Convenio::create(['nome_convenio' =>'Sesc *']);
+Convenio::create(['nome_convenio' =>'Sesi *']);
+Convenio::create(['nome_convenio' =>'Sincottrap *']);
+Convenio::create(['nome_convenio' =>'Sindicato do comercio * ']);
+Convenio::create(['nome_convenio' =>'Sindicato dos estivadores']);
+Convenio::create(['nome_convenio' =>'Sindicato dos rodoviarios']);
+Convenio::create(['nome_convenio' =>'Sindipetro/AP']);
+Convenio::create(['nome_convenio' =>'Sindiviap']);
+Convenio::create(['nome_convenio' =>'Sindrap']);
+Convenio::create(['nome_convenio' =>'Sindtral *']);
+Convenio::create(['nome_convenio' =>'Sinsepeap *']);
+Convenio::create(['nome_convenio' =>'Sorriso saúde *']);
+Convenio::create(['nome_convenio' =>'SSMM *']);
+Convenio::create(['nome_convenio' =>'Stacap *']);
+Convenio::create(['nome_convenio' =>'Sulamerica *']);
+Convenio::create(['nome_convenio' =>'TCM']);
+Convenio::create(['nome_convenio' =>'Tribunal de contas *']);
+Convenio::create(['nome_convenio' =>'Transwood *']);
+Convenio::create(['nome_convenio' =>'UBMA']);
+Convenio::create(['nome_convenio' =>'Vale saude']);
+Convenio::create(['nome_convenio' =>'Visual fashion']);
     }
 }
